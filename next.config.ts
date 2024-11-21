c1d2154e784aa9546333359914c52d688efe7554
Diogo Alves <diogobis23@gmail.com>
2024-11-20 23:56:17 -0300

    Swap NextJS for Svelte

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-  basePath: "diogobis.github.io"
-};
-
-export default nextConfig;
